33b890ba29e4e5a3c2e9f1b4a7b7532832608a9a
MaksymHurianov <11maxim11@ua.fm>
2021-03-06 18:55:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import ReactFileReader from 'react-file-reader'
+import {parse} from 'papaparse'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+        const handleFiles = files => {
+            console.log(files[0].name)
+            let reader = new FileReader();
+            reader.onloadend = function (e){
+                const header = Object.keys(parse(reader.result, {header: true}).data[0])
+                const rows = (parse(reader.result, {header: true}).data)
+                const headerWithIdAndDuplicate = (['id', ...header, 'Duplicate with'])
+                console.log(rows)
+                const rowsWithIdAndDuplicate = rows.map((i, index)=>{
+                    return {id: index, ...i, ['Duplicate with']: ''}
+                })
+                console.log(headerWithIdAndDuplicate)
+                console.log(rowsWithIdAndDuplicate)
+            }
+            reader.readAsText(files[0]);
+        }
+
+    return(
+        <div>
+            <ReactFileReader fileTypes={".csv"} handleFiles={handleFiles}>
+            <button className='btn'>Upload</button>
+        </ReactFileReader>
+        </div>
+    )
+}
 export default App;
