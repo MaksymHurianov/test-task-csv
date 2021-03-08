48c9f92143be895266a7ea847c8ee67e3df118f3
MaksymHurianov <11maxim11@ua.fm>
2021-03-08 20:33:59 +0300

    added validation to email, yearly income, age, phone, license number, expiration date, has children, experiance

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     const [headerWithIdAndDuplicate, setHeaderWithIdAndDuplicate] = useState([])
     const [rowsWithIdAndDuplicate, setRowsWithIdAndDuplicate] = useState([])
     const [isValidFile, setIsValidFile] = useState(true)
+    const [phonesArr, setPhonesArr] = useState([])
 
     const handleFiles = files => {
 
@@ -21,6 +22,7 @@ function App() {
 
         reader.onloadend = function (e) {
             if (reader.result) {
+
 
                 const headerNames = Object.keys(parse(reader.result, {
                     header: true,
@@ -40,12 +42,50 @@ function App() {
                         return i.trim()
                     }
                 }).data)
+                if(headerNames.indexOf('Full Name') === -1 || headerNames.indexOf('Email') === -1 || headerNames.indexOf('Phone') === -1){
+                    setIsValidFile(false)
+                    return
+                }
 
                 const headerWithIdAndDuplicate = (['id', ...headerNames, 'Duplicate with'])
 
+                function phoneValidation(i){
+                    if(headerNames.indexOf('Phone') !== -1){
+                        if(i['Phone'].slice(0,1)==='1' && i['Phone'].length === 11){
+                            i['Phone'] = `+${i['Phone']}`
+
+                        }else if(i['Phone'].length === 10){
+                            i['Phone'] = `+1${i['Phone']}`
+                        }
+                    }
+                }
+                function hasChildrenValidation(i){
+
+                    if(headerNames.indexOf('Has children') !== -1){
+                        if(i['Has children'] === ''){
+                            i['Has children'] = 'FALSE'
+                        } else if((i['Has children'].toUpperCase() === 'FALSE') || (i['Has children'].toUpperCase() === 'TRUE')){
+                            i['Has children'] = i['Has children'].toUpperCase()
+                        }
+                    }
+                }
+                function earlyIncomeValidation(i){
+                    if(headerNames.indexOf('Yearly Income') !== -1){
+                        if(i['Yearly Income'] !== '' && i['Yearly Income'] < 1000000){
+                            i['Yearly Income'] = Number((+i['Yearly Income']).toFixed(2))
+                        }
+                    }
+                }
+
                 const rowsWithIdAndDuplicate = rows.map((i, index) => {
-                    return {id: index, ...i, ['Duplicate with']: ''}
+                    phoneValidation(i)
+                    hasChildrenValidation(i)
+                    earlyIncomeValidation(i)
+                    return {id: index, ...i, 'Duplicate with': ''}
                 })
+                setPhonesArr(rows.map(i=>{
+                    return i['Phone']
+                }))
                 setHeaderWithIdAndDuplicate(headerWithIdAndDuplicate)
                 setRowsWithIdAndDuplicate(rowsWithIdAndDuplicate)
             }else{
@@ -67,7 +107,10 @@ function App() {
                     </ReactFileReader>
                     <table>
                         <Header headerNames={headerWithIdAndDuplicate}/>
-                        <Rows rows={rowsWithIdAndDuplicate} headerNames={headerWithIdAndDuplicate}/>
+                        <Rows rows={rowsWithIdAndDuplicate}
+                              headerNames={headerWithIdAndDuplicate}
+                              phonesArr={phonesArr}
+                        />
                     </table>
                 </>
                 : <div>NO VALID</div>
--- src/Rows.js
+++ src/Rows.js
@@ -1,20 +1,41 @@
 import React from 'react'
 
-function Rows({rows, headerNames}){
+function Rows({rows, headerNames, phonesArr}) {
 
-    return(
+    console.log(phonesArr)
+    console.log(rows)
+    return (
         <tbody>
-        {rows.map((row, index, rows)=>{
 
-            let a = []
-            for(let i=0; i < headerNames.length; ++i){
+        {rows.map((row, index, rows) => {
+            let styledRows = []
+            let stylePhone = {}
+            for (let i = 0; i < headerNames.length; ++i) {
 
-                const style =(row['Age'] < 21 || !+row['Age']) && headerNames[i] === 'Age' ? {backgroundColor: 'red'}: undefined
-                    a = [...a, (<td key={i} style={style}>{rows[index][headerNames[i]]}</td>)]
+                const styleAge = (row['Age'] < 21 || !+row['Age']) && headerNames[i] === 'Age' ? {backgroundColor: 'red'} : undefined
+                const styleExperience = (+row['Experience'] <= 0 || row['Age'] - row['Experience'] < 0) && headerNames[i] === 'Experience' ? {backgroundColor: 'red'} : undefined
+                if (row['Phone']) {
+                    stylePhone = (row['Phone'].length !== 12 || row['Phone'].slice(0, 2) !== '+1') && headerNames[i] === 'Phone' ? {backgroundColor: 'red'} : undefined
+                }else{
+                    stylePhone = headerNames[i] === 'Phone' ? {backgroundColor: 'red'} : undefined
+                }
+                const styleDate = !((/^\d{4}\-\d{1,2}\-\d{1,2}$/.test(row['Expiration date']) || (/(0[1-9]|1[012])[ /.](0[1-9]|[12][0-9]|3[01])[ /.](19|20)\d\d/.test(row['Expiration date'])))
+                    && Date.parse(row['Expiration date']) > Date.now()) && headerNames[i] === 'Expiration date' ? {backgroundColor: 'red'} : undefined
+
+                const styleLicenceNumber = !(/^[a-zA-Z\d]{5}$/).test(row['License number']) && headerNames[i] === 'License number'? {backgroundColor: 'red'} : undefined
+                const styleEmail = !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(row['Email']) && headerNames[i] === 'Email'? {backgroundColor: 'red'} : undefined
+                const styleHasChildren = !((row['Has children'] === ('TRUE')) || (row['Has children'] === ('FALSE'))) && headerNames[i] === 'Has children'? {backgroundColor: 'red'} : undefined
+                const styleYearlyIncome = !(typeof row['Yearly Income'] === "number") && headerNames[i] === 'Yearly Income'? {backgroundColor: 'red'} : undefined
+
+                const styleAll = {...styleAge, ...styleExperience, ...stylePhone, ...styleDate, ...styleLicenceNumber, ...styleEmail, ...styleHasChildren, ...styleYearlyIncome}
+
+
+
+                styledRows = [...styledRows, (<td key={i} style={styleAll}>{rows[index][headerNames[i]]}</td>)]
             }
-            return(
+            return (
                 <tr key={index}>
-                    {a}
+                    {styledRows}
                 </tr>
             )
         })}
@@ -22,4 +43,5 @@ function Rows({rows, headerNames}){
         </tbody>
     )
 }
+
 export default Rows
